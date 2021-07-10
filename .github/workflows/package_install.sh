90835155de5875b9455b04b75a38bb3a0ad65982
Christos Dalamagkas <cdalamagkas@gmail.com>
2021-07-10 15:49:04 +0300

    debconf for Microsoft EULA

--- .github/workflows/package_install.sh
+++ .github/workflows/package_install.sh
@@ -2,8 +2,9 @@
 
 sudo apt-get -qq update
 sudo apt-get install --no-install-recommends -y texlive-base texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-fonts-extra-links texlive-font-utils texlive-plain-generic texlive-xetex texlive-lang-greek lmodern ghostscript
-ACCEPT_EULA=y DEBIAN_FRONTEND=noninteractive \
-sudo apt-get install -y --no-install-recommends ttf-mscorefonts-installer
 
-# echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 
-# echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 
+echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
+
+#ACCEPT_EULA=y DEBIAN_FRONTEND=noninteractive \
+sudo apt-get install -y --no-install-recommends ttf-mscorefonts-installer
