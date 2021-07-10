b0e22b190eb79a8f108437c34d0083263471cd88
Christos Dalamagkas <cdalamagkas@gmail.com>
2021-07-10 15:43:41 +0300

    sudo for second apt-get

--- .github/workflows/package_install.sh
+++ .github/workflows/package_install.sh
@@ -3,7 +3,7 @@
 sudo apt-get -qq update
 sudo apt-get install --no-install-recommends -y texlive-base texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-fonts-extra-links texlive-font-utils texlive-plain-generic texlive-xetex texlive-lang-greek lmodern ghostscript
 ACCEPT_EULA=y DEBIAN_FRONTEND=noninteractive \
-apt-get install -y --no-install-recommends ttf-mscorefonts-installer
+sudo apt-get install -y --no-install-recommends ttf-mscorefonts-installer
 
 # echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 
 # echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
