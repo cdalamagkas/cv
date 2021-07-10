a46d1857f6263473b1319b99fdd540a17308431e
Christos Dalamagkas <cdalamagkas@gmail.com>
2021-07-10 17:27:15 +0300

    attempt to smallcap

--- .github/workflows/package_install.sh
+++ .github/workflows/package_install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sudo apt-get -qq update
-sudo apt-get install --no-install-recommends -y texlive-base texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-fonts-extra-links texlive-font-utils texlive-plain-generic texlive-xetex texlive-lang-greek lmodern ghostscript
+sudo apt-get install --no-install-recommends -y texlive-base texlive-latex-base texlive-latex-extra texlive-fonts-recommended texlive-fonts-extra texlive-fonts-extra-links texlive-font-utils texlive-plain-generic texlive-xetex texlive-lang-greek lmodern ghostscript fonts-gfs-didot
 
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 
-echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
+#echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections 
+#echo ttf-mscorefonts-installer msttcorefonts/present-mscorefonts-eula note | sudo debconf-set-selections
 
 #ACCEPT_EULA=y DEBIAN_FRONTEND=noninteractive \
-sudo apt-get install -y --no-install-recommends ttf-mscorefonts-installer
+#sudo apt-get install -y --no-install-recommends ttf-mscorefonts-installer_all
